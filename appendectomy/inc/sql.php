9e842cbd8823297b6b373ae60506f758538a26a1
sfhuang <huang.shengfeng@gmail.com>
2014-04-08 22:37:51 +0800

    Update sql.php

--- appendectomy/inc/sql.php
+++ appendectomy/inc/sql.php
@@ -8,7 +8,7 @@
 #----------------------------------------------------------------------------------------------------------------------------
 function connect_to_db()
 {
-	if($link = mysql_pconnect("59.125.43.46", "appendectomy","Y298llyY"))
+	if($link = mysql_pconnect("59.125.43.46", DBName, PASSWORD)
 	{
 		mysql_query("SET NAMES 'utf8'");
 		if(mysql_select_db("appendectomy", $link))
